e14be6a8bcfe1537fff6bb76c57276aa8cdc307e
ilyadrbn <mystery@playduck.tech>
2025-11-20 09:38:43 +0200

    initial commit

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+function handleGoogleAnalytics(): void {
+    console.log('Google analytics');
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ $route.params.slug }}</h1>
+        <NuxtLink to="/">Home</NuxtLink>
+        <button
+            @click="handleGoogleAnalytics"
+            type="button">
+            Google analytics
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+    <NuxtLink to="/about">About</NuxtLink>
+</template>
+
+<style lang="scss" scoped></style>
